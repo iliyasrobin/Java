daf90cf069e0f3432cd8bcda72d731c90e123a4a
Iliyas Robin <iliyasahmedr@gmail.com>
2022-10-01 20:55:06 +0600

    Add files via upload

---
+++ ArrayForEachLoop.java
@@ -0,0 +1,15 @@
+
+package javaprograms;
+
+public class ArrayForEachLoop {
+    public static void main(String[] args) {
+          String[] names = {"Iliyas", "Ahmed", "Robin"};
+          
+          //printing using for each loop
+          
+          for (String i : names) {
+              System.out.print(i+" ");
+        }
+          System.out.println();
+    }
+}
